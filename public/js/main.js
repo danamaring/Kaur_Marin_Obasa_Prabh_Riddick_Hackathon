f67f400cd54eeb984383efdaed6224ff10b3d818
Marin Garcia <d_maringarcia@ats-kml11-dm03.fc.ca>
2019-10-07 22:45:00 -0400

    mobile site

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,16 @@
-(() => {
-    console.log('fired!');
+(function(){
+	"use strict";
+	
+	console.log("fired");
+
+	var button = document.querySelector("#button");
+	var burgerCon = document.querySelector("#burgerCon");
+
+	function hamburgerMenu() {
+		burgerCon.classList.toggle("slideToggle");
+		button.classList.toggle("expanded");
+	}
+
+	button.addEventListener("click", hamburgerMenu, false);
+		
 })();
